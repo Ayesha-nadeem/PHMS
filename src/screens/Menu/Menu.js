67828e234076a53fcaa1b3f3ab99927b695e9c9f
ayesha <bsef17m038@pucit.edu.pk>
2020-11-28 16:34:50 +0500

    1

--- src/screens/Menu/Menu.js
+++ src/screens/Menu/Menu.js
@@ -154,7 +154,7 @@ s=(<Items id={this.state.x} hotel={this.props.navigation.getParam('item')} room=
         return (
             <View>
                 
-            <View>
+            <View >
                 <FlatList
                 horizontal
                 // vertical
