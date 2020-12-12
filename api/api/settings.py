b9292bbb4c93dd1273e64090651d950c8ecfe8c9
ayesha <bsef17m038@pucit.edu.pk>
2020-12-12 17:37:40 +0500

    i

--- api/api/settings.py
+++ api/api/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6d8%#r0+$-)ezzqi*odsqvf7c)=dtu2#1mk$c-68zcw4kt9+f!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
